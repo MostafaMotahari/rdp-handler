58a634c69697517502ba3228f038042de8382b5f
Mostafa Motahari <mostafamotahari2004@gmail.com>
2023-08-12 20:14:43 +0330

    reformat png file name.

--- sub_server/endpoints.py
+++ sub_server/endpoints.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from starlette.responses import FileResponse
 
 async def take_screenshot(request):
-    file_name = f"{datetime.now()}.png"
+    file_name = f"{int(datetime.timestamp(datetime.now()))}.png"
     snapshot = ImageGrab.grab()
     snapshot.save(file_name)
 
